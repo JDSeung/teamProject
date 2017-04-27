74d8bd5f840e5647a446d3fc824c2b8754ee14be
SeungYeol Sung <im_s100@naver.com>
2017-04-27 10:31:20 +0900

    ManagerLoginProAction.java 작성

---
+++ src/bookshop/command/ManagerLoginProAction.java
@@ -0,0 +1,29 @@
+package bookshop.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bookshop.bean.MngrDBBean;
+import bookshop.process.CommandAction;
+
+public class ManagerLoginProAction implements CommandAction {
+
+	@Override
+	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		request.setCharacterEncoding("UTF-8");//한글인코딩
+		//넘어온 요청의 데이터를 얻어냄
+		String id = request.getParameter("id");
+		String passwd = request.getParameter("passwd");
+		
+		//DB와 연동해서 사용자의 인증을 처리
+		MngrDBBean dbPro = MngrDBBean.getInstance();
+		int result = dbPro.userCheck(id, passwd);
+		
+		//해당 뷰(응답 페이지)로 보낼 내용을 request 속성에 지정
+		request.setAttribute("result", new Integer(result));
+		request.setAttribute("id", id);
+		
+		return "/jsp/mLoginPro.jsp";
+	}
+
+}
